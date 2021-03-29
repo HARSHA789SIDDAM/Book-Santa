6a0c36d9a0b70409719c53e354ffb24c2f9bbbe0
HARSHA789SIDDAM <siddamharshavardhan@gmail.com>
2021-03-29 16:17:15 +0530

    Created a new Expo app

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity, TextInput, Alert } from 'react-native';
+
+
+export default class WelcomeScreen extends Component {
+    render(){
+        return
+    }
+}
